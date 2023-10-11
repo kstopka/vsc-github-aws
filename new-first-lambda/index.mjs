3822c460a66426dbf145cf7cac06ea52af66ea53
Krystian Stopka <kr6stopka@gmail.com>
2023-10-11 12:24:03 +0200

    new tests deploy

--- new-first-lambda/index.mjs
+++ new-first-lambda/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda! third test"),
+    body: JSON.stringify("Hello from Lambda! third test OK!"),
   };
   return response;
 };
--- new-third-lambda/index.mjs
+++ new-third-lambda/index.mjs
@@ -1,8 +1,14 @@
 export const handler = async (event) => {
   // TODO implement
+
+  console.log("333");
+  console.log("333");
+  console.log("44");
+  console.log("555");
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda! third test"),
+    body: JSON.stringify("Hello from Lambda! third test OK 3"),
   };
   return response;
 };
