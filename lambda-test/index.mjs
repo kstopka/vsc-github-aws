b41d1af14d63e00958f7371fe37c63f2f37a7d9b
Krystian Stopka <kr6stopka@gmail.com>
2023-10-11 22:32:59 +0200

    test

--- lambda-test/index.mjs
+++ lambda-test/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda! Nowy"),
+    body: JSON.stringify("Hello from Lambda! Nowy Test"),
   };
   return response;
 };
