8e7db950160b838bf110c09726ff8ac3fde44262
Krystian Stopka <kr6stopka@gmail.com>
2023-10-11 20:55:33 +0200

    update first lambda

--- new-first-lambda/index.mjs
+++ new-first-lambda/index.mjs
@@ -1,8 +1,11 @@
 export const handler = async (event) => {
   // TODO implement
+
+  const { name } = event;
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda! third test OK!"),
+    body: JSON.stringify("Hello from Lambda! third test OK!" + name),
   };
   return response;
 };
